983e32299d8e0930c6e0b1ebddd786725d4f64c3
anakarlasantana <santanadeanakarla@gmail.com>
2023-08-22 17:50:33 -0300

    feat: ajustes nas credenciais para o banco de dados

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 
 export const AppDataSource = new DataSource({
+    url: process.env.DATABASE_URL,
     type: "postgres",
     host: process.env.DB_HOST,
     port: 6015,
